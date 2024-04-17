15959efcd22c050f7c588ef21674d4e9fafc6064
Weber Jang <weber12599@gmail.com>
2024-04-17 13:45:05 +0800

    Bug fixes
    The bug is about that the obfusecated scripts was seeking an incorrect byte position in Python 3.6.
    The fix ensures that byte seeking now operates correctly.

--- package_obfuscator/file_obfuscator.py
+++ package_obfuscator/file_obfuscator.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import py_compile
 import uuid
 
@@ -8,11 +9,19 @@ import marshal
 import os
 
 s = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '{}', '{}'), 'rb')
-s.seek(16)
+s.seek({})
 exec(marshal.load(s))
 """
 
 
+def _get_header_size():
+    if sys.version_info >= (3, 7):
+        # See: https://peps.python.org/pep-0552/
+        return 16
+    if sys.version_info >= (3, 2):
+        # See: https://www.python.org/dev/peps/pep-3147/
+        return 12
+    return 8
 
 
 def _random_suffix():
@@ -41,7 +50,7 @@ def obfuscate_file(path_to_python, *args, **kwargs):
         renamed_file, cfile=output_pyc_filepath)
 
     # Writing binary parse code to original file
-    new_code = code_template.format(py_cache_folder_name, new_filename_pyc)
+    new_code = code_template.format(py_cache_folder_name, new_filename_pyc, _get_header_size())
     with open(path_to_python, 'w') as f:
         f.write(new_code)
     os.remove(renamed_file)
