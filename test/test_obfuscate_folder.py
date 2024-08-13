9d17ba9d4e65cce6324a4117dc353698842785c3
Megi <mto@maltego.com>
2024-08-13 16:21:39 +0200

    updating readme

--- test/test_obfuscate_folder.py
+++ test/test_obfuscate_folder.py
@@ -52,3 +52,25 @@ def test_in_place_obfuscation():
     assert test_module_output.perform() == 2
     hash_1 = _hash_file(os.path.join(output_dir, 'hello_world.py'))
     assert hash_0 != hash_1
+
+
+def test_obfuscate_folder_with_short_filenames():
+    test_module_name = 'test_module'
+    test_module_name_output = 'test_module_output'
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    output_dir = os.path.join(current_dir, test_module_name_output)
+    if os.path.isdir(output_dir) and os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    assert not os.path.exists(output_dir)
+    package_obfuscator.obfuscate(
+        os.path.join(current_dir, 'test_module'),
+        output=output_dir,
+        force_output_overwrite=True,
+        short_filenames=True
+    )
+    from . import test_module_output
+    assert test_module_output.perform() == 2
+    hash_0 = _hash_file(os.path.join(
+        current_dir, test_module_name, 'hello_world.py'))
+    hash_1 = _hash_file(os.path.join(output_dir, 'hello_world.py'))
+    assert hash_0 != hash_1
